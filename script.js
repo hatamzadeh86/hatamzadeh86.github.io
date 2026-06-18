b808b47d13552fdb4197442414a52ac45cc4fa55
Amir mohammad Hatamzadeh <activedirectoryn@gmail.com>
2026-06-18 13:05:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const skills = [
+"Python",
+"Machine Learning",
+"Artificial Intelligence",
+"Data Science",
+"Git & GitHub",
+"HTML",
+"CSS",
+"JavaScript"
+];
 
+const container = document.getElementById("skills-container");
+
+if(container){
+
+skills.forEach(skill=>{
+
+const card=document.createElement("div");
+
+card.className="skill-card";
+
+card.innerHTML=skill;
+
+container.appendChild(card);
+
+});
+
+}
